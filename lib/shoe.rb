b734eb01b80c93d7ff855a866552c61c32da1e23
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-30 00:11:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,6 +3,14 @@ class Shoe
   attr_accessor :color, :size, :material
   attr_reader :brand
  
+  def condition=(its_condition)
+    @condition = its_condition
+  end
+  
+  def condition
+    @conditon
+  end
+  
   def initialize(brand)
     @brand = brand
   end
